f60d4a61c9e422df4258f909e601aa9ab7191d78
AndyB <andy@purplelama.co.uk>
2019-12-11 12:12:53 +0000

    updated blinker thread to consume rx messages.

--- src/output/outThread.cpp
+++ src/output/outThread.cpp
@@ -6,6 +6,9 @@
 
 #include "main.h"
 #include "log_core.h"
+#include "msg_core.h"
+
+extern mailbox_t rxMailbox;
 
 static THD_WORKING_AREA(waThread1, 32);
 
@@ -14,7 +17,14 @@ static THD_FUNCTION( Thread1, arg) {
     chRegSetThreadName("blinker");
     bool level = false;
     while (true) {
-        chThdSleepMilliseconds(500);
+        uint8_t* rxMsg;
+        uint8_t retVal = chMBFetchTimeout(&rxMailbox, (msg_t*)&rxMsg, 5000);
+        if(retVal == MSG_OK) {
+            // msg consumed
+            msg_free(rxMsg);
+            log_msg(LOG_ALL, "message RX");
+        }
+        // chThdSleepMilliseconds(500);
         level^=1;
         led.write(level);
         log_msg(LOG_ALL, "Heart Beat");
